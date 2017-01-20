717508f84cd50ea049246ccd3e4d96133d98ff34
Diomidis Anadiotis <2284736a@student.gla.ac.uk>
2017-01-20 18:40:30 +0000

    Chapter 3 done

--- rangoProject/rangoProject/urls.py
+++ rangoProject/rangoProject/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url 
-from django.contrib import admin 
-from django.conf.urls import include 
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-url(r'^$', views.index, name='index'), 
-url(r'^rango/', include('rango.urls')), 
+url(r'^$', views.index, name='index'),
+url(r'^rango/', include('rango.urls')),
+url(r'^about/', include('rango.urls')),
+url(r'^admin/', admin.site.urls),
 
 # above maps any URLs starting
 # with rango/ to be handled by
 # the rango application
-
-url(r'^admin/', admin.site.urls),
 ]
