ad4366a9cbefe63d5846bde28671fd2d249b20f8
Diomidis Anadiotis <2284736a@student.gla.ac.uk>
2017-01-26 01:15:28 +0000

    Chapter 6.3

--- rangoProject/rangoProject/settings.py
+++ rangoProject/rangoProject/settings.py
@@ -13,11 +13,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Directories
-#TEMPLATE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
