55652a2e6a9a7f8b59137b2fa867525a11c18ead
Diomidis Anadiotis <2284736a@student.gla.ac.uk>
2017-02-12 17:38:26 +0000

    Update views, added forms. Ch.7

--- rangoProject/rango/views.py
+++ rangoProject/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 
 def index(request):
@@ -27,3 +28,20 @@ def show_category(request, category_name_slug):
 		context_dict['category'] = None
 		context_dict['pages'] = None
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+		form = CategoryForm()
+		if request.method == 'POST':
+        		form = CategoryForm(request.POST)
+        # Have we been provided with a valid form?
+        	if form.is_valid():
+            # Save the new category to the database.
+            		form.save(commit=True)
+            # Now that the category is saved
+            # We could give a confirmation message
+            # But since the most recent category added is on the index page
+            # Then we can direct the user back to the index page.
+            		return index(request)
+        	else:
+				print(form.errors)
+		return render(request, 'rango/add_category.html', {'form': form})
